4fffb9df0eefd581699e81660f9b8c142be92dc3
Onakoya Kayode <onakoyakayode@gmail.com>
2022-06-27 12:35:04 +0100

    change header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "./images/rec-image.png"
+
 
 
 
@@ -7,7 +7,7 @@ import image from "./images/rec-image.png"
 function Header() {
     return (
         <header className="header">
-            {/* <img src={image} className="header-img" alt="header-image" /> */}
+            
         </header>
     )
 }
